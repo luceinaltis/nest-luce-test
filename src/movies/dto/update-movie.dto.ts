36c1d1a89c310b69595c4691102a614727befba4
luceinaltis <luceinaltis@github.com>
2021-07-12 16:41:21 +0900

    update

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,3 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto extends PartialType() {}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
