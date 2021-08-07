e7e23d54c308d6058e9adead4f55198d8604af56
luceinaltis <luceinaltis@github.com>
2021-08-07 13:23:19 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MoviesModule } from './movies/movies.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-    imports: [MoviesModule],
+    imports: [MoviesModule, AdminModule],
     controllers: [AppController],
     providers: [],
 })
